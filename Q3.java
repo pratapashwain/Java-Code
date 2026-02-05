1e41ff60a06a885a4ce2418bd9f803ee96e2f205
pratapashwain <pratapashwain@gmail.com>
2026-02-05 15:38:51 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Q3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter first number = ");
+        int a = sc.nextInt();
+        System.out.print("enter second number =");
+        int b = sc.nextInt();
+         
+        int c = a+b;
+        System.out.println("print the sum of both numbers :"+ c);
+        
+    }
+}
